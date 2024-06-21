fd16c04bbb7475106c3bd2d50f0ed985f688103d
Rramirezch <rodrigora05@yahoo.com>
2024-06-20 21:19:42 -0500

    se sube tarea clase 2

---
+++ ArtistaFavorito/src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+        <h1>MARC ANTONY</h1>
+        <img src='marc-anthony.jpg' alt='Foto Marc Antony' />
+        <p> <b>Marco Antonio Muñiz Rivera</b> (Nueva York, 16 de septiembre de 1968), más conocido
+           como <b><span>Marc Anthony</span></b>, es un cantautor y actor estadounidense, cuyas canciones van 
+           desde la salsa, pasando por el bolero, la balada y el pop latino. Es el artista 
+           de música tropical más exitoso de todos los tiempos.1​Ha ganado cuatro Premios 
+           Grammy, cinco Premios Grammy Latinos y veintinueve Premios Lo Nuestro (cantante 
+           masculino más galardonado). Empezó su trayectoria discográfica en el género del 
+           hiphop con el dúo Little Louie & Marc Anthony, llegando a la cabeza de las listas
+            estadounidenses en 1991. Ralph Mercado lo contrató para su sello RMM en esa fecha
+             y lo lanzó cantando salsa, consiguiendo un gran éxito que duró años, desde su 
+             primer sencillo con la canción «Hasta que te conocí» de Juan Gabriel que fue 
+             número uno en ventas. </p>
+        <p className='pd'>"Texto copiado de Wikipedia"</p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
